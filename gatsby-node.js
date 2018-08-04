35ea25819e12d309c41f3e990dde10c27444896d
Mario Lopez <mario@techmunchies.net>
2018-08-03 21:36:11 -0400

    Moved API out

---
+++ gatsby-node.js
@@ -0,0 +1,118 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+const path = require(`path`)
+const slugify = require(`slugify`)
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const config = require('./gatsby-config')
+
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+  // Query for article nodes to use in creating pages.
+  resolve(
+    graphql(request).then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
+
+      return result
+    })
+  )
+})
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const getProperties = makeRequest(graphql, `
+    {
+      allStrapiProperty {
+        edges {
+          node {
+            id
+            address
+          }
+        }
+      }
+    }
+    `).then(result => {
+    // Create pages for each article.
+    result.data.allStrapiProperty.edges.forEach(({ node }) => {
+      createPage({
+        path: `/properties/${slugify(node.address, { lower: true })}`,
+        component: path.resolve(`src/pages/property.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  })
+
+  // Query for recipe nodes to use in creating pages.
+  return Promise.all([
+    getProperties,
+  ])
+}
+
+exports.onCreateNode = async ({ node, actions, store, cache }) => {
+  const { createNode, createNodeField } = actions
+
+  if ( node.internal.type !== null && node.internal.type === 'StrapiProperty' ) {
+    createNodeField({
+      node,
+      name: 'slug',
+      value: `/properties/${slugify(node.address, { lower: true })}`,
+    })
+
+    createNodeField({
+      node,
+      name: 'seo_description',
+      value:
+        `This home has ${node.pictures.length}` +
+        ' photos of this ' +
+        `${Number(Math.round(node.price + 'e2') + 'e-2').toFixed(2)} ` +
+        `${node.bedrooms} bed, ${node.bathrooms} bath, ` +
+        `${node.home_size} sqft ${node.type.name} located ` +
+        `at ${node.address} built in ${new Date(node.year).getUTCFullYear()}.`,
+    })
+
+    // let images = []
+
+    // for ( const picture of node.pictures ) {
+    //   const fileNode = await createRemoteFileNode({
+    //     url: config.siteMetadata.api + picture.url,
+    //     store,
+    //     cache,
+    //     createNode,
+    //     createNodeId: id => `property-image-${picture.id}`,
+    //   })
+
+    //   if (fileNode) {
+    //     images.push(fileNode)
+    //     //node.images___NODE = fileNode.id
+    //   }
+    // }
+
+    // createNodeField({
+    //   node,
+    //   name: 'images',
+    //   value: images,
+    // })
+  }
+
+  if ( node.internal.type !== null && node.internal.type === 'StrapiForm' ) {
+    const fileNode = await createRemoteFileNode({
+      url: config.siteMetadata.api + node.file.url,
+      store,
+      cache,
+      createNode,
+      createNodeId: id => `form-file-${node.id}`,
+    })
+
+    if (fileNode) {
+      node.localFile___NODE = fileNode.id
+    }
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import Affiliate from '../components/Affiliate'
+import HouseCard from '../components/HouseCard'
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <section className="hero is-small">
+      <div className="hero-body index-hero">
+        <div className="container has-text-centered">
+          <h2 className="title index-subtitle">
+            Our favorite properties
+          </h2>
+
+          {data.allStrapiProperty.edges.length > 0 ?
+            data.allStrapiProperty.edges.map(document => (
+              <HouseCard
+                key={document.node.id}
+                url={document.node.fields.slug}
+                title={document.node.title}
+                cover={document.node.pictures[0].url}
+                price={document.node.price}
+                status={document.node.status.name}
+                address={document.node.address}
+                bedrooms={document.node.bedrooms}
+                bathrooms={document.node.bathrooms}
+                garages={document.node.garages}
+              />
+            )) : (
+            <div className="no-properties-index is-uppercase">
+              <h1>No properties available</h1>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+    <section className="hero is-small">
+      <div className="container has-text-centered">
+        <div className="hero-body">
+          <h2 className="title">Our affiliates</h2>
+          <div className="columns affiliates">
+            {data.allStrapiAffiliate.edges.length > 0 ?
+              data.allStrapiAffiliate.edges.map(document => (
+                <Affiliate
+                  key={document.node.id}
+                  name={document.node.name}
+                  url={document.node.logo.publicURL} />
+              )) : (
+                <div className="no-properties-index is-uppercase">
+                  <h1>No affiliates available</h1>
+                </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiProperty(
+      filter: {
+        favorite: {
+          eq: true
+        }
+      }
+    ) {
+      edges {
+        node {
+          id
+          title
+          pictures {
+            url
+          }
+          price
+          status {
+            name
+          }
+          address
+          bedrooms
+          bathrooms
+          garages
+          fields {
+            slug
+          }
+        }
+      }
+    }
+    allStrapiAffiliate {
+      edges {
+        node {
+          id
+          name
+          logo {
+            publicURL
+          }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        api
+      }
+    }
+  }
+`
+
+export default IndexPage
